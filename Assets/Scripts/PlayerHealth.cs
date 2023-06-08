f3c0151fa4dd1513f17422ca5844710ff94650fb
dakinguyen <n.datquang@gmail.com>
2023-06-08 01:33:35 -0400

    added logic for player to lose health on collision

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class PlayerHealth : MonoBehaviour
 {
     public static event Action<int> HealthChanged =  delegate{};
 
-    private int playerHealth;
+    [SerializeField]
+    private Transform enemyTransform;
+
+    public int playerHealth;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +27,7 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (transform.position == enemyTransform.position)
         {
             DecreaseHealth();
         }
