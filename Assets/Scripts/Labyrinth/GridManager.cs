59874a4bef701a4d429665d63a98467258bf1738
Dante0909 <60408356+Dante0909@users.noreply.github.com>
2023-07-12 20:15:57 -0400

    keys and labyrinth

--- Assets/Scripts/Labyrinth/GridManager.cs
+++ Assets/Scripts/Labyrinth/GridManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -10,6 +11,8 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int gridWidth;
     [SerializeField] private int sizeBetweenRooms;
     [SerializeField] private int numberOfKeys;
+    [SerializeField] private GameObject[] roomPrefabs;
+    [SerializeField] private GameObject corridorPrefab;
     private static GridManager instance = null;
     private Grid grid;
     private Pathfinding pathfinding;
@@ -35,31 +38,39 @@ public class GridManager : MonoBehaviour
 
         bool good = false;
         int safe = 0;
-        do
-        {
-            grid = new Grid(gridWidth, gridHeight, startX, startY, endX, endY);
-            pathfinding = new Pathfinding(grid);
-            good = pathfinding.VerifyInitialGrid();
-            safe++;
-            
-        } while (good == false && safe < 1000);
-
         List<Room> deadEnds = new List<Room>();
 
-        foreach (Room room in grid.GetRooms())
+        do
         {
-            if (room is not null && room.DistanceFromStart != 0)
+            do
             {
-                deadEnds.Add(room);
+                grid = new Grid(gridWidth, gridHeight, startX, startY, endX, endY);
+                pathfinding = new Pathfinding(grid);
+                good = pathfinding.VerifyInitialGrid();
+                safe++;
+
+            } while (good == false && safe < 1000);
+
+            //separate do while to avoid doing this step every time
+
+            foreach (Room room in grid.GetRooms())
+            {
+                if (room is not null && room.DistanceFromStart != 0)
+                {
+                    deadEnds.Add(room);
+                }
+
             }
 
-        }
+        } while (numberOfKeys >= deadEnds.Count);
 
         deadEnds = deadEnds.OrderBy(x => x.DistanceFromStart).ToList();
         int average = Mathf.FloorToInt((float)deadEnds.Count / (float)numberOfKeys);
 
         for (int i = 0; i < numberOfKeys; ++i)
         {
+            int index = i * average;
+            if (index >= deadEnds.Count) throw new Exception("Dead end index problem");
             deadEnds[i * average].ContainsKey = true;
         }
     }
@@ -76,9 +87,112 @@ public class GridManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CreateNewGrid();
+            SpawnPhysicalRooms();
+            SpawnCorridors();
         }
     }
 
+    void SpawnCorridors()
+    {
+        foreach (Corridor c in grid.GetCorridors())
+        {
+            if(c is null) continue;
+            GameObject g = Instantiate(corridorPrefab, c.GetPosition() * sizeBetweenRooms, transform.rotation);
+            if (c.GetRoomA().Y != c.GetRoomB().Y) g.transform.Rotate(Vector3.up, 90);
+        }
+    }
+
+    void SpawnPhysicalRooms()
+    {
+        Vector3 rotation = Vector3.zero;
+        foreach(Room r in grid.GetRooms())
+        {
+            if (r is null) continue;
+            GameObject g = null;
+            int rotate = 0;
+
+            var c = r.GetCorridors();
+            switch (c.Count)
+            {
+                case 1:
+                    g = Instantiate(roomPrefabs[0], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+
+                    if (c.ContainsKey(Room.Cardinal.Bottom))
+                        rotate = 90;
+                    else if (c.ContainsKey(Room.Cardinal.Left))
+                        rotate = 180;
+                    else if (c.ContainsKey(Room.Cardinal.Top))
+                        rotate = 270;
+                    break;
+                case 2:
+                    if (c.ContainsKey(Room.Cardinal.Bottom))
+                    {
+                        if (c.ContainsKey(Room.Cardinal.Top))
+                        {
+                            g = Instantiate(roomPrefabs[2], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+                            rotate = 90;
+                        }
+                        else
+                        {
+                            g = Instantiate(roomPrefabs[1], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+                            rotate = c.ContainsKey(Room.Cardinal.Right) ? 0 : 90;
+                        }
+
+                    }
+                    else if (c.ContainsKey(Room.Cardinal.Top))
+                    {
+                        g = Instantiate(roomPrefabs[1], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+
+                        rotate = c.ContainsKey(Room.Cardinal.Right) ? 270 : 180;
+                    }
+                    else
+                    {
+                        g = Instantiate(roomPrefabs[2], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+                    }
+                    break;
+                case 3:
+                    g = Instantiate(roomPrefabs[3], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+                    if (!c.ContainsKey(Room.Cardinal.Top))
+                        rotate = 90;
+                    else if (!c.ContainsKey(Room.Cardinal.Right))
+                        rotate = 180;
+                    else if (!c.ContainsKey(Room.Cardinal.Bottom))
+                        rotate = 270;
+
+                    break;
+                        default:
+                            g = Instantiate(roomPrefabs[4], new Vector3(r.X * sizeBetweenRooms, 0, r.Y * sizeBetweenRooms), transform.rotation);
+                    break;
+            }
+
+            if (g is not null)
+            {
+                g.transform.Rotate(Vector3.up, rotate);
+                if (!r.ContainsKey && r.GetCorridors().Count == 1)
+                { 
+                    Transform key = RecursiveChildSearch(g.transform, "Key");
+                    if (key is not null) Destroy(key.gameObject);
+                }
+            }
+        }
+    }
+
+    //stupid function to delete the key object
+    private Transform RecursiveChildSearch(Transform parent, string childName)
+    {
+        foreach (Transform child in parent)
+        {
+            if (child.name == childName) return child;
+
+            else
+            {
+                Transform t2 = RecursiveChildSearch(child, childName);
+                if (t2 != null) return t2;
+            }
+        }
+
+        return null;
+    }
     void OnDrawGizmos()
     {
         if (grid is not null)
