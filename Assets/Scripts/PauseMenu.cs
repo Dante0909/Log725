36f13704028d7349bdab4fa3c880829e37247a27
Maximilien <76956953+MaximilienHarvey@users.noreply.github.com>
2023-07-26 16:01:03 -0400

    pause menu only on host

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : NetworkBehaviour
 {
     public static bool isPaused = false;
     public GameObject pauseMenuCanvas;
@@ -18,7 +19,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && IsHost)
         {
             if (optionMenuCanvas.activeSelf)
             {
